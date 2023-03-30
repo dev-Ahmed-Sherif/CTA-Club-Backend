1ee47aa9081f001e284665dca8fa421a4c7c70a7
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-03-30 23:30:50 +0200

    Update Requests.js

--- api/models/Requests.js
+++ api/models/Requests.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 
 const requestSchema = new mongoose.Schema(
@@ -41,47 +41,3 @@ const requestSchema = new mongoose.Schema(
 );
 request = mongoose.model("requests", requestSchema);
 module.exports = request;
-=======
-const mongoose = require("mongoose");
-
-const requestSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      require: true,
-    },
-    date: {
-      type: String,
-      require: true,
-    },
-    from: {
-      type: String,
-      require: true,
-    },
-    to: {
-      type: String,
-      require: true,
-    },
-    number: {
-      type: String,
-      require: true,
-    },
-    vodanumber: {
-      type: String,
-      require: true,
-    },
-    requests: {
-      type: Array,
-    },
-    createdDate: {
-      type: String,
-    },
-  },
-  {
-    versionKey: false,
-    strict: false,
-  }
-);
-request = mongoose.model("requests", requestSchema);
-module.exports = request;
->>>>>>> 9bdc9481de924a76d08003d6dc41bd702dd61194
