f76f0d535fb051a21cc24a7e148f52326772d9e1
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-03-30 23:35:44 +0200

    Update sendMail.js

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mailer = require("nodemailer");
 
 module.exports = async (email, subject, text) => {
@@ -21,27 +21,3 @@ module.exports = async (email, subject, text) => {
     console.log(error, "Email not sent");
   }
 };
-=======
-const mailer = require("nodemailer");
-
-module.exports = async (email, subject, text) => {
-  try {
-    const transporter = mailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "meetingappuni@gmail.com",
-        pass: process.env.Email_PASS,
-      },
-    });
-    await transporter.sendMail({
-      from: "meetingappuni@gmail.com",
-      to: email,
-      subject: subject,
-      text: text,
-    });
-    console.log("Email send successfully");
-  } catch (error) {
-    console.log(error, "Email not sent");
-  }
-};
->>>>>>> 9bdc9481de924a76d08003d6dc41bd702dd61194
