5a3ffed8dd493befaf728f23d23ae21915f63ec2
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-03-30 23:27:31 +0200

    Update mongoose.js

--- api/config/mongoose.js
+++ api/config/mongoose.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -17,23 +17,3 @@ const db = mongoose.connection;
 
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("DB connection is opened"));
-=======
-require("dotenv").config();
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-// mongoose
-//   .connect(
-//     "mongodb+srv://Ahmed:as1me2ba3ah4@football-reservation.2wq2avr.mongodb.net/?retryWrites=true&w=majority",
-//     { useNewUrlParser: true }
-//   )
-//   .then((x) => {
-//     console.log("DB connection is open");
-//   });
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-
-const db = mongoose.connection;
-
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("DB connection is opened"));
->>>>>>> 9bdc9481de924a76d08003d6dc41bd702dd61194
