1626225697bbae5bbcddbd703dab9aaa56ed7ff9
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-03-30 23:30:12 +0200

    Update Clubs.js

--- api/models/Clubs.js
+++ api/models/Clubs.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 
 const clubSchema = new mongoose.Schema(
@@ -39,45 +39,3 @@ const clubSchema = new mongoose.Schema(
 );
 club = mongoose.model("clubs", clubSchema);
 module.exports = club;
-=======
-const mongoose = require("mongoose");
-
-const clubSchema = new mongoose.Schema(
-  {
-    playgroundname: {
-      type: String,
-      require: true,
-    },
-    photoURL: {
-      type: String,
-    },
-    address: {
-      type: String,
-      require: true,
-    },
-    phonenumber: {
-      type: String,
-      require: true,
-    },
-    vodafonnumber: {
-      type: String,
-      require: true,
-    },
-    requests: {
-      type: Array,
-    },
-    table: {
-      type: Array,
-    },
-    createdDate: {
-      type: String,
-    },
-  },
-  {
-    versionKey: false,
-    strict: false,
-  }
-);
-club = mongoose.model("clubs", clubSchema);
-module.exports = club;
->>>>>>> 9bdc9481de924a76d08003d6dc41bd702dd61194
