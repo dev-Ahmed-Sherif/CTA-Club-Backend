7d56fbc098c754190e7b4cc133150f936ce2cff7
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-04-03 12:02:27 +0200

    Update authMiddleware.js

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -4,7 +4,8 @@ const User = require("../models/User");
 require("dotenv").config();
 
 const requireAuth = (req, res, next) => {
-  console.log(req);
+  console.log(req.cookies);
+  console.log(req.headers);
   const token1 = req.cookies.clubToken;
   //const token2 = req.Cookies.clubToken;
   console.log(token1);
