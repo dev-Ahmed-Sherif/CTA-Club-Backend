6209faad283bc10732cea12ec4e343ea36e7485c
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-04-06 11:04:49 +0200

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -182,7 +182,7 @@ router.post("/login", async (req, res) => {
       res.cookie("clubToken", token, {
 //         domain: "cta.onrender.com",
         secure: true,
-        sameSite: "strict",
+        sameSite: "None",
         httpOnly:true,
         path: "/",
         maxAge: maxAge * 1000,
