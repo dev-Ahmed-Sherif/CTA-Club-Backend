f31f97445e6a7b283c2dd8fc6f510b84e8fd9a5f
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-04-14 15:19:14 +0200

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -308,7 +308,7 @@ router.get("/logout", (req, res) => {
       sameSite: "none",
       path: "/",
     });
-  res.redirect(process.env.CLIENT_URL);
+//   res.redirect(process.env.CLIENT_URL);
 });
 
 // router.post(
