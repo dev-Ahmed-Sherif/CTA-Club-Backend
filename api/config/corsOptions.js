271522b7aa83ee450beb65ca8ca4498405e6e2be
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-05-31 19:37:27 +0300

    Update corsOptions.js

--- api/config/corsOptions.js
+++ api/config/corsOptions.js
@@ -4,6 +4,7 @@ console.log(allowedOrigins);
 const corsOptions = {
   
   origin: (origin, callback) => {
+    console.log(origin);
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
