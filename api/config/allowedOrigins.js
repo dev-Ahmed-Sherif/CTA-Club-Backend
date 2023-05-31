b1b577877d2e29e4701d574c3d71bdd559c32a3b
Ahmed Sherif <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-05-31 19:56:51 +0300

    Update allowedOrigins.js

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://cta-football-reservation.netlify.app/",
+  "https://cta-football-reservation.netlify.app",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
